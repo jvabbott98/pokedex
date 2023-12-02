46396307024017491485273f0ee37c2d5061178d
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2023-12-02 18:21:06 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ let pokemonlist = [
     {name: 'Cumbusken', height: 90, type: ['fire', 'fighting']},
     {name: 'Blaziken', height: 190, type: ['fire', 'fighting']}
   ]
-  
+
+  for (let i = 0; i <= 2; i++) {
+    document.write(pokemonlist[i].name, " (height: ", pokemonlist[i].height, ")\n")
+    if (pokemonlist[i].height > 100) {
+        document.write(" - Wow, that's big!")
+    };
+  }
