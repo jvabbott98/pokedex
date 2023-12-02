b72e3d343f53f1608606b0c22b724d62408186a9
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2023-12-02 18:22:51 +0900

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,11 @@ let pokemonlist = [
     {name: 'Cumbusken', height: 90, type: ['fire', 'fighting']},
     {name: 'Blaziken', height: 190, type: ['fire', 'fighting']}
   ]
-
+// Print each pokemon and height
   for (let i = 0; i <= 2; i++) {
     document.write(pokemonlist[i].name, " (height: ", pokemonlist[i].height, ")\n")
     if (pokemonlist[i].height > 100) {
+// Print "Wow, that's big" after pokemon with height over 100
         document.write(" - Wow, that's big!")
     };
   }
