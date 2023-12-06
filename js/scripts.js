1b6661a0c6824c4351f5c8625e780f9eefedaec6
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2023-12-06 22:04:22 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,43 @@ let pokemonList = [
     {name: 'Blaziken', height: 190, type: ['fire', 'fighting']}
   ];
 
+//returns all objects in the pokemonList array.
 return {
     getAll: function() {
         return pokemonList;
     },
+// Adds a pokemon objec to the pokemonList array.
     add: function(pokemon) {
-        pokemonList.push(pokemon);
-    }
+        if (typeof pokemon != "object") {
+            document.write("Argument must be an object");            
+        }else if (Object.keys(pokemon)[0] != "name" || Object.keys(pokemon)[1] != "height" || Object.keys(pokemon)[2] != "type") {
+            document.write(Object.keys(pokemon));
+        }else {
+            pokemonList.push(pokemon); 
+        };
+    },
+// // Attempts to use filter() to search pokemon by name
+//     filtered: function(pokemonName) {
+//         let result = pokemonList.filter(function checkName(value) {
+//     if (value.name == pokemonName) {
+//         return value;
+//     }
+// };)
+//     }
 };
 })();
 
+
+
+pokemonRepository.add({name: "Pikachu", height: 30, type: ['electric']});
+document.write(filtered(torchic));
 
 pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name, " (height: ", pokemon.height, ") ")
     if (pokemon.height > 100) {
 // Print "Wow, that's big" after pokemon with height over 100
-        document.write(" - Wow, that's big!")
+        document.write(" - Wow, that's big! ")
     };
 });
 
- 
+
