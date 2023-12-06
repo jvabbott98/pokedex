1ec27f7bbabacc58658d461c2044ffbf0e29034e
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2023-12-06 20:30:17 +0900

    Update scripts.js
    Added IIfe and forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
-let pokemonlist = [
+let pokemonRepository = (function () {
+let pokemonList = [
     {name: 'Torchic', height: 40, type: ['fire']},
     {name: 'Cumbusken', height: 90, type: ['fire', 'fighting']},
     {name: 'Blaziken', height: 190, type: ['fire', 'fighting']}
-  ]
-// Print each pokemon and height
-  for (let i = 0; i <= 2; i++) {
-    document.write(pokemonlist[i].name, " (height: ", pokemonlist[i].height, ")\n")
-    if (pokemonlist[i].height > 100) {
+  ];
+
+return {
+    getAll: function() {
+        return pokemonList;
+    },
+    add: function(pokemon) {
+        pokemonList.push(pokemon);
+    }
+};
+})();
+
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name, " (height: ", pokemon.height, ") ")
+    if (pokemon.height > 100) {
 // Print "Wow, that's big" after pokemon with height over 100
         document.write(" - Wow, that's big!")
     };
-  }
+});
+
+ 
